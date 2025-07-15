07b00bebdc8acd65cffbe175634706c179c095b5
Aishwarya Dhaul <aishwarya.dhaul28@gmail.com>
2025-07-15 17:29:58 +0200

    Added Authentication Setup

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/user_model.dart';
+
+class AuthProvider extends ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  
+  User? _user;
+  UserModel? _userModel;
+  bool _isLoading = false;
+  
+  User? get user => _user;
+  UserModel? get userModel => _userModel;
+  bool get isLoading => _isLoading;
+  bool get isAuthenticated => _user != null;
