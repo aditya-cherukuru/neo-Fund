d4eb84340e2a9532318c5833a08ca572969764cb
Aishwarya Dhaul <aishwarya.dhaul28@gmail.com>
2025-07-20 14:59:28 +0200

    created login screen widget and basic stateful structure

---
+++ lib/screens/auth/login_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
+import '../../providers/auth_provider.dart';
+import '../../utils/theme.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _nameController = TextEditingController();
+  bool _isSignUp = false;
+  bool _isLoading = false;
