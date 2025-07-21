4fa0b6e350342819a184839b1ba29275f2728365
Aishwarya Dhaul <aishwarya.dhaul28@gmail.com>
2025-07-21 10:36:01 +0200

    initial setup for profile screen

---
+++ lib/screens/profile/profile_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../providers/auth_provider.dart';
+import '../../utils/theme.dart';
+import '../../widgets/bottom_nav_bar.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
